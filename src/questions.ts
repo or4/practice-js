069ebba45d69935eab22b06cc2c603f270c05c54
Dmitry Vakarev <dmitryvakarev@gmail.com>
2019-08-11 01:28:49 +0700

    Develop (#1)
    * add jest
    
    * add examples
    
    * add easy tasks
    
    * Добавил задачи

---
+++ src/questions.ts
@@ -0,0 +1,22 @@
+// Что такое цикл событий?
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/EventLoop
+
+// Что такое всплытие и перехват (event bubbling)
+// https://learn.javascript.ru/event-bubbling
+
+// Что такое замыкание?
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Closures
+
+// Как работает прототипное наследование и чем оно отличается от классической модели наследования?
+// Иногда спрашивают, но редко вопрос в приоритете
+// Прототипное наследование https://developer.mozilla.org/ru/docs/Learn/JavaScript/Объекты/Inheritance
+
+// Что такое контекст. Как работает ключевое слово this? Разница function и стрелочными функциями
+
+// Что такое hoisting
+
+// Что такое REST?
+
+// Мой сайт тормозит. Расскажите о шагах по его диагностированию и исправлению.
+
+// Расскажите о методах call, apply, bind
---
+++ src/type-c/c3.get.ts
@@ -0,0 +1,21 @@
+/*
+ * Задача c3: Написать упрощенную версию функции get как в примере
+ * Напишите тесты.
+ */
+
+/*
+	Примеры:
+	const obj = {
+		a: {
+			b: {
+				c: 25;
+			}
+		}
+	};
+
+	get(obj, 'a.b.c') // 25
+	get(obj, 'a.b') // {c: 25}
+	get(obj, 'a.b.d') // undefined
+	get(obj, 'a.b.d.e') // undefined
+	get(obj, 'a.b.d.e.f.c.e.d.c') // undefined
+*/
