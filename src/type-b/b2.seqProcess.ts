b1c920a42f6d505f8080776b83b25c497505ba97
vakarevds <vakarevds@yandex-team.ru>
2019-08-28 13:32:59 +0700

    b2.seqProcess

--- src/type-b/b2.seqProcess.ts
+++ src/type-b/b2.seqProcess.ts
@@ -5,3 +5,40 @@
  * Когда все запросы обработаны, функция возвращает массив с резульататами.
  * Напишите тесты.
  */
+
+async function getResults1(urls: string[]) {
+	const output = [];
+
+	for (const url of urls) {
+		const result = await fetch(url);
+
+		output.push(result);
+	}
+
+	return output;
+}
+
+function getResults2(urls: string[]) {
+	return new Promise(resolve => {
+		const output: object[] = [];
+
+		function processResult(data: object) {
+			output.push(data);
+			const urlLocal = urls.shift();
+
+			if (urlLocal) {
+				fetch(urlLocal).then(processResult);
+			} else {
+				resolve(output);
+			}
+		}
+
+		const url = urls.shift();
+
+		if (url) {
+			fetch(url).then(processResult);
+		} else {
+			resolve(output);
+		}
+	});
+}
